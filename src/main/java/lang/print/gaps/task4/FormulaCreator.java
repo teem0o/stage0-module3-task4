11ba45d75c28259ef0b4c3afff70a0be1a1ea4c1
temo <chxaidzetemo@gmail.com>
2022-07-07 20:38:17 +0400

    hhh

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+
+        String data = "(9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2)";
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,12 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c = 'c', h = 'h', a = 'a', r = 'r';
+        int cInt = c, hInt = h, aInt = a, rInt = r;
+        //99 104 97 114
+        System.out.println(cInt);
+        System.out.println(hInt);
+        System.out.println(aInt);
+        System.out.println(rInt);
     }
 }
